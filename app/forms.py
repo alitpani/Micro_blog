052d67cf8783fa613a05b158a5098ce947fcb264
alit <alitpani@gmail.com>
2021-05-13 19:44:48 +0530

    Fourth Commit User Avatar

--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import ValidationError, DataRequired, Email, EqualTo
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, TextAreaField
+from wtforms.fields.simple import TextAreaField
+from wtforms.validators import Length, ValidationError, DataRequired, Email, EqualTo, Length
 from app.models import User
 class LoginForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
@@ -23,6 +24,13 @@ class RegistrationForm(FlaskForm):
         user = User.query.filter_by(email = email.data).first()
         if user is not None:
             raise ValidationError('please use a different username')
+
+class EditProfileForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    about_me = TextAreaField('About Me',validators=[Length(min=0,max=140)])
+    submit = SubmitField('Submit')
+
+
 
 
 
